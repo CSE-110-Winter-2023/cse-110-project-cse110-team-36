82ba1b00329810ab956d8b71de3d1c3645f84798
박채연 <chay@bagchaeyeon-ui-MacBook-Pro.local>
2023-02-09 17:18:13 -0800

    sensor

--- app/src/main/java/edu/ucsd/cse110/lab4/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/lab4/MainActivity.java
@@ -2,21 +2,105 @@ package edu.ucsd.cse110.lab4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private Sensor mMagneticField;
+    private float mAzimut,mPitch,mRoll;
+    private TextView mResultView;
+    private ImageView imageView;
+    private float DegreeStart = 0f;
+    private ImageView compassimage;
+
+    float[] mGravity;
+    float[] mGeomagnetic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mResultView = (TextView) findViewById(R.id.coordinateTextView);
+        imageView = findViewById(R.id.rotatingImageView1);
+        // initialize your android device sensor capabilities
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mMagneticField = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        compassimage = (ImageView) findViewById(R.id.compassImage);
     }
 
-    public void onLaunchListClicked(View view) {
-        Intent intent = new Intent(this, ProfileActivity.class);
-        startActivity(intent);
-        finish();
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mMagneticField, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+                SensorManager.SENSOR_DELAY_GAME);
     }
+
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+            mGravity = event.values;
+        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
+            mGeomagnetic = event.values;
+        if (mGravity != null && mGeomagnetic != null) {
+            float R[] = new float[9];
+            float I[] = new float[9];
+            boolean success = SensorManager.getRotationMatrix(R, I, mGravity, mGeomagnetic);
+            if (success) {
+                float orientation[] = new float[3];
+                SensorManager.getOrientation(R, orientation);
+                mAzimut = (float)Math.toDegrees(orientation[0]);
+                mPitch = (float)Math.toDegrees(orientation[1]);
+                mRoll = (float)Math.toDegrees(orientation[2]);
+
+                String result;
+                result = "Azimut:"+mAzimut+"\n"+"Pitch:"+mPitch+"\n"+"Roll:"+mRoll;
+                mResultView.setText(result);
+                imageView.setRotation(360-mAzimut);
+            }
+        }
+
+        // get angle around the z-axis rotated
+        float degree = Math.round(event.values[0]);
+        //DegreeTV.setText("Heading: " + Float.toString(degree) + " degrees");
+        // rotation animation - reverse turn degree degrees
+        RotateAnimation ra = new RotateAnimation(
+                DegreeStart,
+                -degree,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+        // set the compass animation after the end of the reservation status
+        ra.setFillAfter(true);
+        // set how long the animation for the compass image will take place
+        ra.setDuration(210);
+        // Start animation of compass image
+        compassimage.startAnimation(ra);
+        DegreeStart = -degree;
+    }
+
 }
+
+//    public void onLaunchListClicked(View view) {
+//        Intent intent = new Intent(this, ProfileActivity.class);
+//        startActivity(intent);
+//        finish();
+//    }
