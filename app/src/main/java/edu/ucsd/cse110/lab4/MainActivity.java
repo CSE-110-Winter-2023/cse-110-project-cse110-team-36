1e97d6bf82b9c432e63fe3f81c94844761a86370
Kailey <kwilkens@ucsd.edu>
2023-02-11 16:50:43 -0800

    added OrientationService, getting (questionably accurate) azimuth

--- app/src/main/java/edu/ucsd/cse110/lab4/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/lab4/MainActivity.java
@@ -3,16 +3,34 @@ package edu.ucsd.cse110.lab4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import java.lang.Math;
 
 public class MainActivity extends AppCompatActivity {
+
+    OrientationService orientationService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        orientationService = new OrientationService(this);
+        TextView orientation = findViewById(R.id.orientationView);
+
+        orientationService.getOrientation().observe(this, angle -> {
+            orientation.setText(Float.toString(angle));
+        });
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        orientationService.unregisterSensorListeners();
+    }
+
 
     public void onLaunchListClicked(View view) {
         Intent intent = new Intent(this, ProfileActivity.class);
