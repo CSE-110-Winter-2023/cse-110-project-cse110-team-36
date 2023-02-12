938431b6e88de697dded718acdbdb1f0e3b078cd
Kailey <kwilkens@ucsd.edu>
2023-02-11 18:27:21 -0800

    compass rotating

--- app/src/main/java/edu/ucsd/cse110/lab4/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/lab4/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import java.lang.Math;
 
@@ -18,10 +20,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         orientationService = new OrientationService(this);
+        ImageView compass = findViewById(R.id.compass_base);
         TextView orientation = findViewById(R.id.orientationView);
 
         orientationService.getOrientation().observe(this, angle -> {
-            orientation.setText(Float.toString(angle));
+            compass.setRotation(360 - (float) (Math.toDegrees(angle)));
+            orientation.setText(Float.toString((float) (Math.toDegrees(angle))));
         });
     }
 
