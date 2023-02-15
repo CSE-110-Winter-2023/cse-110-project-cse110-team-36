43e2499416c1c8b5c01587ce1d854e8ca3b9c837
Thy Nguyen <tcn002@ucsd.edu>
2023-02-14 23:16:37 -0800

    yaml

---
+++ app/src/test/java/edu/ucsd/cse110/lab4/labelUnitTests.java
@@ -0,0 +1,51 @@
+
+
+package edu.ucsd.cse110.lab4;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.lifecycle.Lifecycle;
+import androidx.test.core.app.ActivityScenario;
+
+import org.junit.Rule;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import android.content.Intent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+//import org.robolectric.RobolectricTestRunner;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Pair;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import java.util.List;
+
+import android.hardware.SensorEventListener;
+//import androidx.test.rule.GrantPermissionRule;
+
+@RunWith(RobolectricTestRunner.class)
+public class labelUnitTests {
+    @Test
+    public void test_basic_label() {
+
+    }
+}
