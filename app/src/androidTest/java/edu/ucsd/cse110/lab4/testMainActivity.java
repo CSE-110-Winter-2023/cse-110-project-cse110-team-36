10f2aeb5566bb19f3343a9987926472969e7ef05
Thy Nguyen <tcn002@ucsd.edu>
2023-02-18 21:41:08 -0800

    test MainActivity

--- app/src/androidTest/java/edu/ucsd/cse110/lab4/testMainActivity.java
+++ app/src/androidTest/java/edu/ucsd/cse110/lab4/testMainActivity.java
@@ -1,5 +1,7 @@
 package edu.ucsd.cse110.lab4;
+
 import static android.content.Context.LOCATION_SERVICE;
+import static android.content.Context.MODE_PRIVATE;
 import static android.location.LocationManager.GPS_PROVIDER;
 
 import androidx.annotation.NonNull;
@@ -13,12 +15,15 @@ import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 
+import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.preference.PreferenceManager;
 import android.util.Pair;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.lifecycle.LiveData;
@@ -31,6 +36,9 @@ import androidx.test.rule.GrantPermissionRule;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static java.security.AccessController.getContext;
+
+import android.content.SharedPreferences;
 
 @RunWith(AndroidJUnit4.class)
 public class testMainActivity {
@@ -44,53 +52,45 @@ public class testMainActivity {
                     "android.permission.ACCESS_FINE_LOCATION");
 
     @Test
-    public void testSaveButton() {
-        ActivityScenario<ProfileActivity> scenario = ActivityScenario.launch(ProfileActivity.class);
+    public void testInstanceVariables() {
+        ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class);
+        scenario.moveToState(Lifecycle.State.CREATED);
+        scenario.moveToState(Lifecycle.State.STARTED);
+        scenario.onActivity(activity -> {
+            assertTrue(activity.orientationService instanceof OrientationService);
+            assertTrue(activity.locationService instanceof LocationService);
+
+        });
+    }
+    @Test
+    public void testLoadProfileInMain() {
+        ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class);
         scenario.moveToState(Lifecycle.State.CREATED);
         scenario.moveToState(Lifecycle.State.STARTED);
         scenario.onActivity(activity -> {
 
-        activity.loadProfile(); //  load user profile
+            Context context = activity.getApplicationContext();
+            SharedPreferences preferences = context.getSharedPreferences("coordinates", MODE_PRIVATE);
+            String mockOrientationString = preferences.getString("mockOrientation", "");
+            assertFalse(activity.mockedOrientation);
+            activity.loadProfile(); //  load user profile
 
-        TextView lat  = (TextView) activity.findViewById(R.id.latitude);
-        TextView lon  = (TextView) activity.findViewById(R.id.longitude);
-
-        lat.setText("0.0");
-        lon.setText("0.0");
-
-        assertEquals("0.0", lat.getText().toString()); // get initial long/lat
-        assertEquals("0.0", lon.getText().toString());
-
-        // perform click
-        Button btn;
-        btn = (Button) activity.findViewById(R.id.saveButton);
-        boolean clicked = btn.performClick();
-
-        assertTrue(clicked); // assert if clicked
-
-        // launch next screen
-        Intent intent = new Intent(activity, ProfileActivity.class);
-        activity.startActivity(intent);
+            if (!mockOrientationString.equals("")) {
+                assertTrue(activity.mockedOrientation);
+            } else {
+                assertFalse(activity.mockedOrientation);
+            }
 
         });
     }
 
     @Test
-    public void testEditLongLat() {
-        // launch new page
-        ActivityScenario<ProfileActivity> scenario_1 = ActivityScenario.launch(ProfileActivity.class);
-        scenario_1.moveToState(Lifecycle.State.CREATED);
-        scenario_1.moveToState(Lifecycle.State.STARTED);
-        scenario_1.onActivity(activity_1 -> {
-            activity_1.loadProfile();
-            TextView lat_1  = (TextView) activity_1.findViewById(R.id.latitude);
-            TextView lon_1  = (TextView) activity_1.findViewById(R.id.longitude);
-
-            lat_1.setText("0.05");
-            lon_1.setText("-0.01");
-
-            assertEquals("0.05", lat_1.getText().toString()); // initial long/lat
-            assertEquals("-0.01", lon_1.getText().toString());
+    public void onPauseWorking() {
+        ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class);
+        scenario.moveToState(Lifecycle.State.CREATED);
+        scenario.moveToState(Lifecycle.State.STARTED);
+        scenario.onActivity(activity -> {
+            activity.onPause();
         });
     }
 }
